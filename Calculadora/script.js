745ec1aa3037ad01d3a4890d67619c390fdae608
Helder650 <helderblm09@gmail.com>
2024-09-04 17:37:59 -0300

    Update script.js

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,5 +1,5 @@
-let number1 = parseInt(prompt("Digite um número."))
-let number2 = parseInt(prompt("Digite outro número."))
+let number1 = 5
+let number2 = 2
 
 
 let soma = number1 + number2
@@ -8,8 +8,10 @@ let multiplicacao = number1 * number2
 let divisao = number1 / number2
 
 
-alert(`Resultados:`);
-alert(`${number1} + ${number2} = ${soma}`)
-alert(`${number1} - ${number2} = ${subtracao}`)
-alert(`${number1} * ${number2} = ${multiplicacao}`)
-alert(`${number1} / ${number2} = ${divisao}`)
+console.log(`Resultado`)
+console.log(`${number1} + ${number2} = ${soma}`)
+console.log(`${number1} - ${number2} = ${subtracao}`)
+console.log(`${number1} * ${number2} = ${multiplicacao}`)
+console.log(`${number1} / ${number2} = ${divisao}`)
+
+
