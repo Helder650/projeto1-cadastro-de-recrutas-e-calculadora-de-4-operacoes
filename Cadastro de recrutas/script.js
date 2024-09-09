02142941b079001bea4e0f171ef6cce76aa0fac0
Helder650 <helderblm09@gmail.com>
2024-09-09 17:55:32 -0300

    Delete Cadastro de recrutas directory

--- Cadastro de recrutas/script.js
+++
@@ -1,7 +0,0 @@
-let nome = "John"
-let sobrenome = "Wick"
-let curso = "Desenvolvimento de Sistemas
-let anoNascimento = 1989
-
-console.log('Olá ' + nome + ' ' + sobrenome + ', então você estuda ' + curso + ' e tem ' + (2024 - anoNascimento) + ' anos. Legal!')
-
