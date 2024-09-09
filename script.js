edbdf5885ff37470074fe74325c789a7e76c5ba9
Helder650 <helderblm09@gmail.com>
2024-09-09 17:56:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Cadastro de recrutas
+let nome = "John"
+let sobrenome = "Wick"
+let curso = "Desenvolvimento de Sistemas"
+let anoNascimento = 1989
+
+console.log('Olá ' + nome + ' ' + sobrenome + ', então você estuda ' + curso + ' e tem ' + (2024 - anoNascimento) + ' anos. Legal!')
+
+// Calculadora Simples
+let number1 = 5
+let number2 = 2
+
+
+let soma = number1 + number2
+let subtracao = number1 - number2
+let multiplicacao = number1 * number2
+let divisao = number1 / number2
+
+
+console.log(`Resultado`)
+console.log(`${number1} + ${number2} = ${soma}`)
+console.log(`${number1} - ${number2} = ${subtracao}`)
+console.log(`${number1} * ${number2} = ${multiplicacao}`)
+console.log(`${number1} / ${number2} = ${divisao}`)
